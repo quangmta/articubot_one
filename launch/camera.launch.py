ba4465d37770ac08ebb8628201593d296655ee2a
quangmta <nguyennhuquang1998@gmail.com>
2023-12-31 00:42:43 +0300

    uncomment

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -16,6 +16,5 @@ def generate_launch_description():
                                    'depth_module.profile': '424x240x6',
                                    'rgb_camera.profile': '320x180x30',
                                    'align_depth.enable':'true'}.items()
-                                   
         )
     ])
