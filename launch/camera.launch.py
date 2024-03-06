52fc4872ede3f0826b96c5e9901ad427e5b2994e
quangmta <nguyennhuquang1998@gmail.com>
2024-03-06 13:35:38 +0100

    work in jetson

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 def generate_launch_description():
 
@@ -13,9 +14,19 @@ def generate_launch_description():
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('realsense2_camera'),'launch','rs_launch.py'
             )]), launch_arguments={'pointcloud.enable':'true',
-                                   'pointcloud.ordered_pc': 'true',
+                                   'enable_gyro':'true',
+                                   'enable_accel':'true',
+                                   'unite_imu_method':'2',
+                                #    'pointcloud.ordered_pc': 'true',
                                    'depth_module.profile': '424x240x6',
                                    'rgb_camera.profile': '320x240x6',                                   
                                    'align_depth.enable':'true'}.items()
-        )
+                                   
+        ),
+        Node(
+                package="imu_filter_madgwick",
+                executable="imu_filter_madgwick_node",
+                parameters=[{'use_mag': False}],
+                remappings=[('/imu/data_raw','/camera/imu')]
+            )
     ])
