7926fe9c0f5340cb96039e2deae644fa7a67cee5
quangmta <nguyennhuquang1998@gmail.com>
2024-03-20 15:59:38 +0300

    change to humble

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -63,7 +63,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -76,7 +76,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
