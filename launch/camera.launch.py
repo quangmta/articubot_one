0a4dd4110a4b78cf8a44f300328937147aa87493
quangmta <nguyennhuquang1998@gmail.com>
2024-03-29 12:08:41 +0100

    update params

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('realsense2_camera'),'launch','rs_launch.py'
-            )]), launch_arguments={'pointcloud.enable':'true',
+            )]), launch_arguments={'pointcloud.enable':'false',
                                    'enable_gyro':'true',
                                    'enable_accel':'true',
                                    'unite_imu_method':'2',
