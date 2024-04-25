f856cead6426f9c5d828b0d67ec04ee3aaa5cd6e
quangmta <nguyennhuquang1998@gmail.com>
2024-04-25 14:09:55 +0200

    add kf filters

---
+++ launch/rtan_map_rgbd_sync.launch.py
@@ -0,0 +1,130 @@
+# Requirements:
+#   Install Turtlebot3 packages
+#   Modify turtlebot3_waffle SDF:
+#     1) Edit /opt/ros/$ROS_DISTRO/share/turtlebot3_gazebo/models/turtlebot3_waffle/model.sdf
+#     2) Add
+#          <joint name="camera_rgb_optical_joint" type="fixed">
+#            <parent>camera_rgb_frame</parent>
+#            <child>camera_rgb_optical_frame</child>
+#            <pose>0 0 0 -1.57079632679 0 -1.57079632679</pose>
+#            <axis>
+#              <xyz>0 0 1</xyz>
+#            </axis>
+#          </joint> 
+#     3) Rename <link name="camera_rgb_frame"> to <link name="camera_rgb_optical_frame">
+#     4) Add <link name="camera_rgb_frame"/>
+#     5) Change <sensor name="camera" type="camera"> to <sensor name="camera" type="depth">
+#     6) Change image width/height from 1920x1080 to 640x480
+#     7) Note that we can increase min scan range from 0.12 to 0.2 to avoid having scans 
+#        hitting the robot itself
+# Example:
+#   $ export TURTLEBOT3_MODEL=waffle
+#   $ ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+#
+#   SLAM:
+#   $ ros2 launch rtabmap_demos turtlebot3_rgbd_sync.launch.py
+#   OR
+#   $ ros2 launch rtabmap_launch rtabmap.launch.py visual_odometry:=false frame_id:=base_footprint subscribe_scan:=false  approx_sync:=true approx_rgbd_sync:=false odom_topic:=/odom args:="-d --RGBD/NeighborLinkRefining true --Reg/Strategy 1 --Reg/Force3DoF true --Grid/RangeMin 0.2" use_sim_time:=true rgbd_sync:=true rgb_topic:=/camera/color/image_raw depth_topic:=/estimated_depth camera_info_topic:=/camera/color/camera_info qos:=2
+#   $ ros2 run topic_tools relay /rtabmap/map /map
+#
+#   Navigation (install nav2_bringup package):
+#     $ ros2 launch nav2_bringup navigation_launch.py use_sim_time:=True
+#     $ ros2 launch nav2_bringup rviz_launch.py
+#
+#   Teleop:
+#     $ ros2 run turtlebot3_teleop teleop_keyboard
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    qos = LaunchConfiguration('qos')
+    localization = LaunchConfiguration('localization')
+    queue = LaunchConfiguration('queue_size')
+
+    parameters={
+          'frame_id':'base_footprint',
+          'use_sim_time':use_sim_time,
+          'subscribe_rgbd':True,
+          'subscribe_scan':True,
+          'use_action_for_goal':True,
+          'qos_scan':qos,
+          'qos_image':qos,
+          'qos_imu':qos,
+          'queue_size':queue,
+          'approx_sync':True,
+        #   'approx_sync_max_interval':0.5,
+          # RTAB-Map's parameters should be strings:
+          'Reg/Strategy':'1',
+          'Reg/Force3DoF':'true',
+          'RGBD/NeighborLinkRefining':'True',
+          'Grid/RangeMin':'0.2', # ignore laser scan points on the robot itself
+          'Optimizer/GravitySigma':'0' # Disable imu constraints (we are already in 2D)
+    }
+
+    remappings=[
+          ('scan','/scan'),
+          ('odom','/odom'),
+        #   ('rgb/image', '/camera/color/image_raw'),
+          ('rgb/image', '/original_sync_rgb_image'),
+        #   ('rgb/image', '/rgb_image_sync'),
+          ('rgb/camera_info', '/camera/color/camera_info'),
+          ('depth/image', '/processed_depth_image'),
+        #   ('depth/image', '/original_sync_depth_image'),
+        #   ('depth/image', '/estimated_depth')
+        ]
+
+    return LaunchDescription([
+
+        # Launch arguments
+        DeclareLaunchArgument(
+            'use_sim_time', default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        
+        DeclareLaunchArgument(
+            'qos', default_value='2',
+            description='QoS used for input sensor topics'),
+            
+        DeclareLaunchArgument(
+            'localization', default_value='false',
+            description='Launch in localization mode.'),
+        
+        DeclareLaunchArgument(
+            'queue_size', default_value='500',
+            description='Queue size.'),
+
+        # Nodes to launch
+        Node(
+            package='rtabmap_sync', executable='rgbd_sync', output='screen',
+            parameters=[{'approx_sync':True, 'use_sim_time':use_sim_time, 'qos':qos,
+                         'queue_size':queue}],
+            remappings=remappings),
+
+        # SLAM Mode:
+        Node(
+            condition=UnlessCondition(localization),
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=[parameters],
+            remappings=remappings,
+            arguments=['-d']),
+            
+        # Localization mode:
+        Node(
+            condition=IfCondition(localization),
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=[parameters,
+              {'Mem/IncrementalMemory':'False',
+               'Mem/InitWMWithAllNodes':'True'}],
+            remappings=remappings),
+
+        Node(
+            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+            parameters=[parameters],
+            remappings=remappings),
+    ])
